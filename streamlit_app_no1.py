65e13b99f171b4c5ae70c4d32db2819924b3976e
Sanyo <114914631+SanyoSFtest@users.noreply.github.com>
2022-10-03 22:57:48 +0200

    Add header and text

--- streamlit_app_no1.py
+++ streamlit_app_no1.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("Hello, world!")
 
-streamlit.write("This is a poem \
-I try the second raw")
+streamlit.header("This is a poem")
+streamlit.text ("I try the second raw")
 streamlit.write("And there is a new raw at least!")
