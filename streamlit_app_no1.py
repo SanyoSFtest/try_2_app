b32b77c1066450680f1ad3bd19b31096441b5bbf
Sanyo <114914631+SanyoSFtest@users.noreply.github.com>
2022-10-03 23:04:48 +0200

    Add another header

--- streamlit_app_no1.py
+++ streamlit_app_no1.py
@@ -8,3 +8,5 @@ streamlit.text("🍞 And the soup needs a bread")
 streamlit.text("🥗 Then we eat a big salad")
 streamlit.text("🐔 With a vegetarian chicken")
 streamlit.text("🥑 Avocado is a very great dessert!")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
